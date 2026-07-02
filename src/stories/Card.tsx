f9e1e3d5abb7c4dcd7a6e4077ed42d05c135821e
lywoo <lywoo.sroeng@gmail.com>
2026-07-02 14:55:29 +0700

    chore: add tailwind css

--- src/stories/Card.tsx
+++ src/stories/Card.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 export default function Card({children}: React.PropsWithChildren) {
   return (
-    <div className="">
+    <div className="rounded-lg border border-slate-200 bg-white px-6 py-4 text-slate-900 shadow-sm">
       {children}
     </div>
   );
